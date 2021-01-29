067d6d852a1bbe03772eaf9c40e14c6902779cb9
Luis Miguel Tolentino Caraballo <luistolentino27@gmail.com>
2021-01-28 20:59:44 -0400

    defining button comp

---
+++ src/components/Button.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <button className="calc-button">1</button>
+            </div>
+        )
+    }
+}
+
+export default Button;
