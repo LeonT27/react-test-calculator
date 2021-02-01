15519c254ece2de1f3a6975a234a8a5296cf67d3
Luis Miguel Tolentino Caraballo <luistolentino27@gmail.com>
2021-02-01 18:46:42 -0400

    add style and func

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,8 +7,8 @@ class Button extends Component {
 
     render() {
         return (
-            <div>
-                <button className="calc-button">{this.props.symbol}</button>
+            <div className={`cols-${this.props.cols}`}>
+                <button className="calc-button" onClick={() => this.props.action(this.props.symbol)}>{this.props.symbol}</button>
             </div>
         )
     }
